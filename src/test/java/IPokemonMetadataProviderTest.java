092f3490c27a744a0159f3e45d5e61f7eb74af37
jo <msimion.pro@gmail.com>
2023-02-27 16:22:53 +0100

    ajout du test pour le metadata provider

---
+++ src/test/java/IPokemonMetadataProviderTest.java
@@ -0,0 +1,31 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mockito;
+
+public class IPokemonMetadataProviderTest {
+  static IPokemonMetadataProvider mockedProvider;
+  @BeforeAll public static void initialize() {
+    mockedProvider = Mockito.mock(IPokemonMetadataProvider.class);
+  }
+  @Test public void testGetPokemonMetadata() throws PokedexException {
+    PokemonMetadata bulbasaurMetadata = new PokemonMetadata(1, "Bulbasaur", 49, 49, 45);
+    Mockito.when(mockedProvider.getPokemonMetadata(1)).thenReturn(bulbasaurMetadata);
+
+    PokemonMetadata expectedPokemonMetadata = mockedProvider.getPokemonMetadata(1);
+
+    assertEquals(expectedPokemonMetadata, bulbasaurMetadata);
+    Mockito.verify(mockedProvider).getPokemonMetadata(1);
+  }
+  @Test public void testGetInnexistantPokemonMetadata() throws PokedexException {
+    Mockito.when(mockedProvider.getPokemonMetadata(722)).thenThrow(new PokedexException("ce pokemon n’existe pas encore"));
+
+    assertThrows(PokedexException.class, () -> {
+      mockedProvider.getPokemonMetadata(722);
+    });
+    Mockito.verify(mockedProvider).getPokemonMetadata(722);
+  }
+}
