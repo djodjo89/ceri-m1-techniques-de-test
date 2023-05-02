a5a83ab73599fe5ecdb5c7d312532435a27d772c
djodjo89 <mathysimion@yahoo.fr>
2023-05-02 14:25:02 +0200

    finaliser l’implémentation

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -4,56 +4,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
-import org.mockito.Mockito;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class IPokedexTest {
-  static IPokedex mockedPokedex;
+  static IPokedex pokedex;
   static Pokemon charizard;
-  @BeforeAll public static void initialize() {
-    mockedPokedex = Mockito.mock(IPokedex.class);
-    charizard = new Pokemon(6, "charizard", 84, 78, 100, 3000, 78, 8000, 8, 100.0);
+  @BeforeAll public static void initialize() throws PokedexException {
+    IPokemonMetadataProvider provider = new PokemonMetadataProvider(
+        new HashSet<PokemonMetadata>(List.of(
+            new PokemonMetadata(1, "bulbasaur", 49, 49, 45)
+        )));
+    IPokemonFactory factory = new PokemonFactory(provider);
+    pokedex = new Pokedex(provider, factory);
+    charizard = factory.createPokemon(1, 3000, 78, 8000, 8);
   }
   @Test public void testSize() {
-    Mockito.when(mockedPokedex.size()).thenReturn(0);
-
-    int actual = mockedPokedex.size();
+    int actual = pokedex.size();
 
     assertEquals(0, actual);
-    Mockito.verify(mockedPokedex).size();
   }
   @Test public void testAddPokemon() {
-    Mockito.when(mockedPokedex.addPokemon(charizard)).thenReturn(1);
-
-    int actual = mockedPokedex.addPokemon(charizard);
+    int actual = pokedex.addPokemon(charizard);
 
     assertEquals(1, actual);
-    Mockito.verify(mockedPokedex).addPokemon(charizard);
   }
   @Test public void testGetPokemon() throws PokedexException {
-    Mockito.when(mockedPokedex.getPokemon(1)).thenReturn(charizard);
-
-    Pokemon actual = mockedPokedex.getPokemon(1);
+    pokedex.addPokemon(charizard);
+    Pokemon actual = pokedex.getPokemon(1);
 
     assertEquals(charizard, actual);
-    Mockito.verify(mockedPokedex).size();
   }
   @Test public void testGetNonexistentPokemon() throws PokedexException {
-    Mockito.when(mockedPokedex.getPokemon(0)).thenThrow(new PokedexException("ce pokemon n’existe pas"));
-
     assertThrows(PokedexException.class, () -> {
-      mockedPokedex.getPokemon(0);
+      pokedex.getPokemon(0);
     });
-    Mockito.verify(mockedPokedex).getPokemon(0);
   }
   @Test public void testGetPokemons() {
-    Mockito.when(mockedPokedex.getPokemons()).thenReturn(new ArrayList<Pokemon>());
+    List<Pokemon> actual = pokedex.getPokemons();
 
-    List<Pokemon> actual = mockedPokedex.getPokemons();
-
-    assertEquals(0, actual.size());
-    Mockito.verify(mockedPokedex).getPokemons();
+    assertEquals(1, actual.size());
   }
 }
 
