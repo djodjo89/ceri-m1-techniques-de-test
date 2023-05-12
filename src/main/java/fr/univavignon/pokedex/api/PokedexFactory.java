59379315159ae3839cc74d92310e6bfb11110b3c
djodjo89 <mathysimion@yahoo.fr>
2023-05-12 11:33:24 +0200

    ajouter spotless et checkstyle pour formater les fichiers

--- src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
@@ -1,11 +1,1 @@
-package fr.univavignon.pokedex.api;
 
-/**
- * Factory class for a simple creation of Pokedex
- *
- */ 
-public class PokedexFactory implements IPokedexFactory {
-	public IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
-		return new Pokedex(metadataProvider, pokemonFactory);
-	}
-}
--- src/main/java/fr/univavignon/pokedex/api/Team.java
+++ src/main/java/fr/univavignon/pokedex/api/Team.java
@@ -1,15 +1,1 @@
-package fr.univavignon.pokedex.api;
 
-/**
- * Pokemon Team enumeration.
- * @author fv
- */
-public enum Team {
-
-	MYSTIC,
-
-	INSTINCT,
-	
-	VALOR
-
-}
